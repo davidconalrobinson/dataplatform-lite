8c73bbea3547a29b973e97f067ebd54bf63a0f3a
davidconalrobinson <44108384+davidconalrobinson@users.noreply.github.com>
2021-08-21 16:08:07 +1000

    Added table and column descriptions

--- src/database/objects.py
+++ src/database/objects.py
@@ -14,9 +14,14 @@ from src.config_loader import schema, objects
 # Loop through objects and generate a class for each.
 for k0, v0 in objects.items():
 	object_dict={
-		'__table_args__': {'schema' : schema},
+		'__table_args__': {
+			'schema' : schema,
+			'comment': v0['description']},
 		'__tablename__': k0
 	}
-	for k1, v1 in objects[k0].items():
-		object_dict.update({k1: Column(eval(v1['dtype']), primary_key=v1['primary_key'] if 'primary_key' in v1 else False)})
+	for k1, v1 in v0['columns'].items():
+		object_dict.update({k1: Column(
+			eval(v1['dtype']),
+			comment=v1['comment'],
+			primary_key=v1['primary_key'] if 'primary_key' in v1 else False)})
 	vars()[k0]=type(k0, (Base, ), object_dict)
