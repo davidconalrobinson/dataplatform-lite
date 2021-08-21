c763c6985401828e909ef24928c9facb5483eff3
davidconalrobinson <44108384+davidconalrobinson@users.noreply.github.com>
2021-08-21 16:37:15 +1000

    Added uuid extension

--- src/database/create.py
+++ src/database/create.py
@@ -9,12 +9,17 @@ from src.database.objects import *
 from src.config_loader import schema
 
 
-def create_schema(Base, schema):
+def before_create(Base, schema):
 	"""
-	Create schema before creating database.
+	Steps before creating database:
+		- create schema
+		- enable uuid extension
 	"""
 	def before_create(target, connection, **kwargs):
-		sql=text('CREATE SCHEMA IF NOT EXISTS {schema};'.format(schema=schema))
+		sql=text("""
+			CREATE SCHEMA IF NOT EXISTS {schema};
+			CREATE EXTENSION "uuid-ossp";
+			""".format(schema=schema))
 		connection.execute(sql)
 	event.listen(Base.metadata, 'before_create', before_create)
 
@@ -23,7 +28,7 @@ def create_all(Base, schema, engine):
 	"""
 	Create schema and database.
 	"""
-	create_schema(Base, schema)
+	before_create(Base, schema)
 	Base.metadata.create_all(engine)
 
 
