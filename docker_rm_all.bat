1e97615b656a1ce8a7fb997ddcbae3691453bf9a
davidconalrobinson <44108384+davidconalrobinson@users.noreply.github.com>
2021-08-29 11:58:41 +1000

    Update docker_rm_all.bat

--- docker_rm_all.bat
+++ docker_rm_all.bat
@@ -6,8 +6,8 @@ docker container rm dataplatform-lite-db-infra
 docker container rm dataplatform-lite-db-src
 docker container rm dataplatform-lite-viz
 docker container rm dataplatform-lite-api
-docker image rm xero-case-study_db_infra
-docker image rm xero-case-study_db_src
-docker image rm xero-case-study_viz
-docker image rm xero-case-study_api
+docker image rm dataplatform-lite_db_infra
+docker image rm dataplatform-lite_db_src
+docker image rm dataplatform-lite_viz
+docker image rm dataplatform-lite_api
 pause
